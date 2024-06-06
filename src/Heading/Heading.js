1cf4861ed20b29a2a3d116bb22b0b1e8fbe6d10f
Matthew Marshall <marsh189@mail.chapman.edu>
2024-06-06 12:04:41 -0700

    Fixed Heading.js

--- src/Heading/Heading.js
+++ src/Heading/Heading.js
@@ -2,9 +2,9 @@ import './heading.css'
 const Heading = (props) => {
   return (
     <section className="heading-section">
-      <img src={props.img} alt={this.props.img} />
-      <h1>{props.title}</h1>
-      <p>{props.desc}</p>
+      <img src={props?.img} alt={props?.img} />
+      <h1>{props?.title}</h1>
+      <p>{props?.desc}</p>
     </section>
   )
 }
